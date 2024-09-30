dbe899475d2ceb10bcf755ee4dcf70765d47c62e
CHOSEONGJUN <lionjojo@naver.com>
2024-09-30 14:46:50 +0900

    init: Create Project - 캘린더 프로젝트 생성

---
+++ src/main/java/com/sparta/calendarprojects/CalendarProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.calendarprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarProjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarProjectsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/calendarprojects/CalendarProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.calendarprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarProjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
